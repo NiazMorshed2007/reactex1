fc1f593e166fbad1bdb1835efd9e23b54ef4058c
Niaz Morshed <niazmorshed2007@gmail.com>
2021-08-31 12:03:07 +0600

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,35 @@
-import React from "react";
+import React, { Component } from "react";
 import "./style/style.scss";
-import Slider from "./components/Slider";
+// import Slider from "./components/Slider";
 
-function App() {
-  return (
-    <div className="App">
-      <Slider />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { count: 0 };
+  }
+
+  inc = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  dec = () => {
+    this.setState({ count: this.state.count - 1 });
+  };
+
+  rst = () => {
+    this.setState({ count: 0 });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <div className="result">{this.state.count}</div>
+        <button onClick={this.inc}>inc</button>
+        <button onClick={this.dec}>dec</button>
+        <button onClick={this.rst}>rst</button>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,24 +1,27 @@
-import React, { useRef } from "react";
+// import React, { Component } from "react";
 
-function Slider() {
-  var slider = useRef();
-  console.log(slider);
-  const slideArr = [
-    "https://images.unsplash.com/photo-1586348943529-beaae6c28db9?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8bmF0dXJlfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
-    "https://images.unsplash.com/photo-1426604966848-d7adac402bff?ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8bmF0dXJlfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
-    "https://images.unsplash.com/photo-1418065460487-3e41a6c84dc5?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fG5hdHVyZXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
-    "https://images.unsplash.com/photo-1469474968028-56623f02e42e?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fG5hdHVyZXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
-    "https://images.unsplash.com/photo-1472214103451-9374bd1c798e?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fG5hdHVyZXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
-  ];
-  return (
-    <>
-      {slideArr.map((e, i) => (
-        <div ref={slider} key={i} className="slide">
-          <img src={e} alt="" />
-        </div>
-      ))}
-    </>
-  );
-}
+// class Slider extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = { count: 0 };
+//   }
 
-export default Slider;
+//   inc() {
+//     this.setState({ count: this.state.count + 1 });
+//   }
+
+//   dec() {
+//     this.setState({ count: this.state.count - 1 });
+//   }
+
+//   render() {
+//     return (
+//       <>
+//         <button onClick={this.inc}>inc</button>
+
+//         <div className="result">{this.state.count}</div>
+//       </>
+//     );
+//   }
+// }
+// export default Slider;
